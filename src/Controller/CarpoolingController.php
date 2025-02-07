58a3ffc6afc6c4433f7b30fdd6fd5464095743f0
YutaOkkotsu94 <lakhdari.fares@outlook.fr>
2025-02-07 20:43:59 +0100

    Change the form method to POST in carpooling template, inclusion of names in the form.Create the variable (depart, arrivee, date and passagers, change the function findAll to dinByfilters for send the variable in data base. Create a function findByfilters in the carpoolingrepository to call the variables of database and get a response from the database

--- src/Controller/CarpoolingController.php
+++ src/Controller/CarpoolingController.php
@@ -10,18 +10,33 @@ use App\Repository\CarpoolingRepository;
 
 final class CarpoolingController extends AbstractController
 {
+
+
     #[Route('/carpooling', name: 'app_carpooling')]
     public function search(): Response
     {
         return $this->render('carpooling/search.html.twig');
     }
 
+#[Route('/contact', name:'contatc')]
+public function contact(): Response
+{
+    return $this->render('contact/contact.html.twig');
+}
+
+
     #[Route('/carpooling/list', name: 'app_carpooling_list')]
     public function list(Request $request, CarpoolingRepository $carpoolingRepository): Response
     {
-        dd($request);
-        return $this->render('carpooling/list.html.twig', [
-            'carpoolings' => $carpoolingRepository->findAll(),
+        $depart = $request->request->get('depart');
+        $arrivee = $request->request->get('arrivee');
+        $date = $request->request->get('date');
+        $passagers = $request->request->get('passagers');
+
+        $carpoolings = $carpoolingRepository->findByFilters($depart,$arrivee,$date,$passagers);
+
+        return $this->redirectToRoute('app_carpooling_list', [
+            'carpoolings' => $carpoolings,
         ]);
     }
 }
--- src/Repository/CarpoolingRepository.php
+++ src/Repository/CarpoolingRepository.php
@@ -40,4 +40,36 @@ class CarpoolingRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
+
+public function findByFilters(?string $depart, ?string $arrivee, ?string $date, ?int $passagers)
+{
+    $qb = $this->createQueryBuilder('carpooling');
+
+    if ($depart) {
+        $qb->andWhere('carpooling.place_of_departure = :depart')
+           ->setParameter('depart', $depart);
+    }
+
+    if ($arrivee) {
+        $qb->andWhere('carpooling.place_of_arrival = :arrivee')
+           ->setParameter('arrivee', $arrivee);
+    }
+
+    if ($date) {
+        $startDate = new \DateTime($date);
+        $endDate = (clone $startDate)->modify('+1 day');
+    
+        $qb->andWhere('carpooling.departure_date >= :startDate')
+           ->andWhere('carpooling.departure_date < :endDate')
+           ->setParameter('startDate', $startDate)
+           ->setParameter('endDate', $endDate);
+    }
+
+    if ($passagers) {
+        $qb->andWhere('carpooling.number_of_place >= :passagers')
+           ->setParameter('passagers', $passagers);
+    }
+
+    return $qb->getQuery()->getResult();
+}
 }
