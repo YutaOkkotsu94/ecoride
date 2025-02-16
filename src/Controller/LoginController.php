fe20123cde24ce9230a87b8560c967b235272f9f
YutaOkkotsu94 <lakhdari.fares@outlook.fr>
2025-02-16 21:57:06 +0100

    Rename connection to LoginController, creating links between pages, insert images, delete the search page, modifying of css

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class SearchController extends AbstractController
+final class LoginController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
+    #[Route('/login', name: 'app_login')]
     public function index(): Response
     {
-        return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
         ]);
     }
 }
